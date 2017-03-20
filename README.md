629a3bf880523a4fbbfeb9f139906d15fd7da67f
ygneo <antonio.barcia@gmail.com>
2017-03-20 23:49:08 +0100

    First approach to setup process

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # jarduino
+
+Requirements
+---
+
+- wget
+- python 2.7
+- pip
+
+Setup
+--
+
+It has been tested in Ubuntu 16.10
+
+ - Install requirements.
+
+- Run:
+
+```
+$ ./setup.sh
+```
+
+It will install jarduino in ~/.jarduino
+
+
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+JARDUINO_HOME_DIR=~/.jarduino
+ARDUINO_MAKEFILE_VERSION="1.5"
+ARDUINO_IDE_VERSION="1.8.1"
+ARDUINO_PLATFORM="linux64"
+ARDUINO_DIRNAME=arduino-$ARDUINO_IDE_VERSION
+
+mkdir -p $JARDUINO_HOME_DIR
+mkdir -p $JARDUINO_HOME_DIR/tmp
+
+
+# Install Arduino IDE
+rm $JARDUINO_HOME_DIR/tmp/*
+ext="tar.xz"
+file=$ARDUINO_DIRNAME-$ARDUINO_PLATFORM.$ext
+wget https://downloads.arduino.cc/$file -P $JARDUINO_HOME_DIR/tmp/
+tar xf $JARDUINO_HOME_DIR/tmp/$file -C $JARDUINO_HOME_DIR
+$JARDUINO_HOME_DIR/$arduino/install.sh
+
+# Install Arduino Makefile
+rm $JARDUINO_HOME_DIR/tmp/*
+ext="tar.gz"
+file=$ARDUINO_MAKEFILE_VERSION.$ext
+ARDMK_DIR = Arduino-MakeFile-$ARDUINO_MAKEFILE_VERSION
+cd $JARDUINO_HOME_DIR
+wget https://github.com/sudar/Arduino-Makefile/archive/$file -P $JARDUINO_HOME_DIR/tmp/
+tar xf $JARDUINO_HOME_DIR/tmp/$file -C $JARDUINO_HOME_DIR
+
+pip install pyserial
+
+rm $JARDUINO_HOME_DIR/tmp/*
+
+echo "# Jarduino env variables" >> ~/.bashrc
+echo "export ARDUINO_DIR=$ARDUINO_HOME_DIR/$ARDUINO_DIRNAME" >> ~/.bashrc
+echo "export ARDMK_DIR=$ARDUINO_HOME_DIR$ARDMK_DIR" >> ~/.bashrc
+source ~/.bashrc
